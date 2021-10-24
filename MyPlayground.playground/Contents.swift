14ea228b7f9ad57584460a73c4ffe15c4309b29f
Jesus Rios <jesusriosdev@gmail.com>
2021-10-23 19:05:17 -0700

    variables constants, multiple assignation

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground";
+//var greeting = "Hello, playground";
+//print("hello juanga!")
 
-print("hello juanga!")
+// Constant: DOESNT CHANGE
+let maximumNumberOfLoginAttempts = 3
+
+// Variable: CAN CHANGE
+var currentLoginAttemps = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
